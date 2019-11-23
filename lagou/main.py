2c69fe2c1f690c6eaddbc308e6484ad14735d48c
CrayonShinChan <471129477@qq.com>
2019-11-23 10:33:08 +0800

    main

---
+++ lagou/main.py
@@ -0,0 +1,9 @@
+import scrapy.cmdline as cmd
+
+# cmd.execute("scrapy crawl positionCategory".split())
+cmd.execute('scrapy crawl positionCategory'.split())
+#
+# import os
+#
+# os.chdir('spiders')
+# cmd.execute('scrapy runspider lagouSpider.py'.split())
